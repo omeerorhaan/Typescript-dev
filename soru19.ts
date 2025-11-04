405a0ce0f37740056383ba7c5e5a4c34546af4d6
omeerorhaan <omer.orhan.aslan@gmail.com>
2025-11-05 00:33:46 +0300

    soru19.ts

---
+++ soru19.ts
@@ -0,0 +1,10 @@
+type Flatten<T> = T extends (infer U)[] ? U : T;
+type NumArray = number[];
+type Str = string;
+
+let el1: Flatten<NumArray> = 123;
+let el2: Flatten<Str> = "hello";
+// @ts-expect-error - 'NumArray'in elemanı 'number', 'string' değil
+let errorEl: Flatten<NumArray> = "string";
+
+console.log("Soru 19 Başarılı!", el1, el2);
